82764dd27c577564555d418ab9ff7fe454bcc7b3
apelttom <apelttom@gmail.com>
2014-11-09 22:41:37 +0000

    NEW: Login obrazovka

--- HIFI-prototyp/gen/com/example/hifi_prototyp/R.java
+++ HIFI-prototyp/gen/com/example/hifi_prototyp/R.java
@@ -1649,11 +1649,13 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
-        public static final int action_settings=0x7f090041;
+        public static final int action_settings=0x7f090046;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
+        public static final int button_login=0x7f090045;
+        public static final int button_login_passw_reset=0x7f090044;
         public static final int checkbox=0x7f09002b;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f090031;
@@ -1661,6 +1663,8 @@ containing a value of this type.
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
         public static final int dropdown=0x7f09001a;
+        public static final int editText_login_passw=0x7f090043;
+        public static final int editText_login_username=0x7f090041;
         public static final int edit_query=0x7f090035;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090024;
@@ -1696,6 +1700,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003e;
         public static final int tabMode=0x7f09000c;
+        public static final int textView_login_title=0x7f090042;
         public static final int title=0x7f090029;
         public static final int top=0x7f09001f;
         public static final int up=0x7f090001;
@@ -1814,7 +1819,12 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
         public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000f;
+        public static final int application_title=0x7f0a0012;
         public static final int hello_world=0x7f0a0010;
+        public static final int login_button_passw_reset_title=0x7f0a0015;
+        public static final int login_button_title=0x7f0a0016;
+        public static final int login_passw_hint=0x7f0a0014;
+        public static final int login_username_hint=0x7f0a0013;
     }
     public static final class style {
         /** 
