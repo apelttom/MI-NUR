999bd59eac1fa79883681f8a55f37986f9974e80
bicanjir <bicanjir@fit.cvut.cz>
2014-11-17 04:15:39 +0100

    vyber karty

--- HIFI-prototyp/src/com/example/hifi_prototyp/PickCard.java
+++ HIFI-prototyp/src/com/example/hifi_prototyp/PickCard.java
@@ -3,46 +3,65 @@ package com.example.hifi_prototyp;
 import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
 
 public class PickCard extends Activity {
 
-	public static String picked = "";
+	private int x,y;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pick_card);
+		
+		ImageView img = (ImageView)findViewById(R.id.imageView1);
+		img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+            	x = (int)event.getX();
+        	    y = (int)event.getY();
+        		return false;
+            }
+		});
 	}
 	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
+	public void pickCard(View view) {
 		Display display = getWindowManager().getDefaultDisplay();
 		Point size = new Point();
 		display.getSize(size);
 		int width = size.x;
 		int height = size.y;
-		
-		int x = (int)event.getX();
-	    int y = (int)event.getY();
-	    
-	    int clickX = x / (width / 13);
+		view.getX();
+		int clickX = x / (width / 13);
 	    int clickY = y / (height / 4);
-		return true;
-	}
-
-	public void pickCard(View view) {
-//		view
+	    Log.d("pick card", "x="+clickX+", y="+clickY+", size="+width+"x"+height+", touch x="+x+", y="+y);
+	    
+	    String card = "";
+	    switch (clickY) {
+	    case 0: card = "h"; break;
+	    case 1: card = "c"; break;
+	    case 2: card = "d"; break;
+	    case 3: card = "s"; break;
+	    }
+	    switch (clickX) {
+	    case 9: card = card+"j"; break;
+	    case 10: card = card+"q"; break;
+	    case 11: card = card+"k"; break;
+	    case 12: card = card+"1"; break;
+	    default: card = card + String.valueOf(clickX+2);
+	    }
+	    PokerTable.setCard(card);
+	    Log.d("pick card", "picked "+card);
+	    finish();
 	}
 	
 	/** Called when the user clicks the Back reset button */
 	public void closeActivity(View view) {
+		Log.d("pick card", "BACK");
 		finish();
 	}
 }
--- HIFI-prototyp/src/com/example/hifi_prototyp/PokerTable.java
+++ HIFI-prototyp/src/com/example/hifi_prototyp/PokerTable.java
@@ -1,12 +1,15 @@
 package com.example.hifi_prototyp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class PokerTable extends Activity {
+	
+	private static ImageButton active = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,41 @@ public class PokerTable extends Activity {
 		setContentView(R.layout.activity_poker_table);
 	}
 	
+	public void pickFlop1(View view) {
+		Intent displayPasswResetForm = new Intent(this, PickCard.class);
+		startActivity(displayPasswResetForm);
+		active = (ImageButton)findViewById(R.id.imageButton_card_flop_1);
+	}
+	public void pickFlop2(View view) {
+		Intent displayPasswResetForm = new Intent(this, PickCard.class);
+		startActivity(displayPasswResetForm);
+		active = (ImageButton)findViewById(R.id.imageButton_card_flop_2);
+	}
+	public void pickFlop3(View view) {
+		Intent displayPasswResetForm = new Intent(this, PickCard.class);
+		startActivity(displayPasswResetForm);
+		active = (ImageButton)findViewById(R.id.imageButton_card_flop_3);
+	}
+	public void pickTurn(View view) {
+		Intent displayPasswResetForm = new Intent(this, PickCard.class);
+		startActivity(displayPasswResetForm);
+		active = (ImageButton)findViewById(R.id.imageButton_card_turn);
+	}
+	public void pickRiver(View view) {
+		Intent displayPasswResetForm = new Intent(this, PickCard.class);
+		startActivity(displayPasswResetForm);
+		active = (ImageButton)findViewById(R.id.imageButton_card_river);
+	}
+	
 	/** Called when the user clicks the Back reset button */
 	public void closeActivity(View view) {
 		finish();
+	}
+
+	public static void setCard(String card) {
+		if (active != null) {
+			active.setImageURI(Uri.parse("android.resource://com.example.hifi_prototyp/drawable//"+card));
+			active = null;
+		}
 	}
 }
